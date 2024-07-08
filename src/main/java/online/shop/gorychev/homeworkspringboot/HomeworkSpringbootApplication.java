4c90cc93b055c09bb2f4a9f14db849f19ea20354
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-07-08 23:57:11 +0300

    started coding

---
+++ src/main/java/online/shop/gorychev/homeworkspringboot/HomeworkSpringbootApplication.java
@@ -0,0 +1,13 @@
+package online.shop.gorychev.homeworkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSpringbootApplication.class, args);
+	}
+
+}
